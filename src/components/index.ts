d4743a63baea809074f8f4cbceb955291c9c08df
Anarya Indika Putra <anaryaindikaputra@Anaryas-MacBook-Pro.local>
2023-10-01 22:07:28 +0700

    Refactor codes

--- src/components/index.ts
+++ src/components/index.ts
@@ -35,12 +35,3 @@ export { Text } from '@components/typography/text';
 
 // Inputs
 export { Button } from '@components/inputs/button';
-
-// Data Display
-// export { default as VerticalTimeline } from '@/components/verticalTimeline';
-// export { default as VerticalTimelineConnector } from '@/components/verticalTimelineConnector';
-// export { default as VerticalTimelineContent } from '@/components/verticalTimelineContent';
-// export { default as VerticalTimelineSeparator } from '@/components/VerticalTimelineSeparator/VerticalTimelineSeparator';
-// export { default as VerticalTimelineDot } from '@/components/verticalTimelineDot';
-// export { default as VerticalTimelineOppositeContent } from '@/components/verticalTimelineOppositeContent';
-// export { default as VerticalTimelineItem } from '@/components/verticalTimelineItem';
